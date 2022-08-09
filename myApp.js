bc3a191ac8d3d68ff055ac039df330d71424f001
SaimouliBandari <saimouli4k@gmail.com>
2022-08-09 22:03:15 +0530

    model.find()

--- myApp.js
+++ myApp.js
@@ -52,8 +52,11 @@ const findOneByFood = function(food, done) {
   });
 };
 
-const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+const findPersonById = function(personId, done) {
+  Person.findById(personId, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
